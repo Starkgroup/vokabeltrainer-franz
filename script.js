130ad6fa3a99bc347106febc47bdf45b0631076b
Starkgroup <org@starkgroup.de>
2024-11-02 16:20:45 +0100

    Revert "test remove apiKey"
    This reverts commit a4d755bd1685addfa969dabe9b47792d454865b8.

--- script.js
+++ script.js
@@ -408,11 +408,6 @@ if (!storedApiKey) {
 }
 
 async function initializeApp() {
-
-    function deleteApiKey() {
-        localStorage.removeItem('apiKey');
-    }
-    
     // Retrieve languages from localStorage
     const userLanguage = localStorage.getItem('userLanguage');
     const trainingLanguage = localStorage.getItem('trainingLanguage');
